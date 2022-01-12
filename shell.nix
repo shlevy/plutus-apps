c797581ecce546ffe28e33428d884ca4605e660b
Jamie Bertram <jamie.bertram@tweag.io>
2022-01-12 14:29:14 -0500

    Add nami wallet generated code

--- shell.nix
+++ shell.nix
@@ -95,6 +95,7 @@ let
     haskell-language-server-wrapper
     hie-bios
     hlint
+    pab-nami-demo.generate-purescript
     pab-nami-demo.start-backend
     plutus-playground.generate-purescript
     plutus-playground.start-backend
